017d6b5fae1fbcdb0ba94911639cf4263832b137
zhongwei <40238143@qq.com>
2019-07-27 15:47:40 +0800

    Add testReplaceAll

--- ExcelToWord/src/test/java/com/hyj/ExcelToWord/WordUtilTest.java
+++ ExcelToWord/src/test/java/com/hyj/ExcelToWord/WordUtilTest.java
@@ -2,6 +2,10 @@ package com.hyj.ExcelToWord;
 
 import org.apache.log4j.Logger;
 
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -33,7 +37,6 @@ public class WordUtilTest extends TestCase {
 	 */
 	public void testApp() {
 		assertTrue(true);
-		log.info("### xxx");
 	}
 /*
 	public static void testApp1() throws Exception {
@@ -53,7 +56,7 @@ public class WordUtilTest extends TestCase {
 
 		log.info("#### done");
 	}
-*/
+
 	public static void testApp2() throws Exception {
 		log.info("#### start");
 		WordUtil word = new WordUtil();
@@ -63,11 +66,48 @@ public class WordUtilTest extends TestCase {
 		word.close();
 		log.info("#### done");
 	}
+*/
+	public static void testReplaceAll() {
+		ActiveXComponent wordApp = new ActiveXComponent("Word.Application");
+		wordApp.setProperty("Visible", new Variant(false)); // 不可见打开word
+		wordApp.setProperty("AutomationSecurity", new Variant(3)); // 禁用宏
+
+		Dispatch docs = wordApp.getProperty("Documents").toDispatch();
+
+		Dispatch doc = Dispatch.call(docs, "Open", TEST_FILE_NAME).toDispatch();
+		Dispatch selection = Dispatch.get(wordApp, "Selection").toDispatch();
+
+		while (find(wordApp, selection, REPLACE_STR_1)) {
+			Dispatch.put(selection, "Text", REPLACE_STR_1_NEW);
+			Dispatch.call(selection, "MoveRight");
+		}
+
+		Dispatch.call(Dispatch.call(wordApp, "WordBasic").getDispatch(), "FileSaveAs", TEST_FILE_NAME_1);
+
+		//Dispatch.call(doc, "Save");
+		//Dispatch.call(doc, "Close", new Variant(true));
+		Dispatch.call(wordApp, "Quit");
+	}
 
 	private static String TEST_FILE_NAME = "E:\\hyj\\jacobTest.doc";
+	private static String TEST_FILE_NAME_1 = "E:\\hyj\\jacobTest_1.doc";
 
 	private static String REPLACE_STR_1 = "##主题##";
 	private static String REPLACE_STR_1_NEW = "3.14";
 
-	private static String TEST_FILE_NAME_1 = "E:\\hyj\\jacobTest_1.doc";
+	private static boolean find(ActiveXComponent wordApp, Dispatch selection, String text) {
+		boolean ret = false;
+		if (text == null || text.equals("")) {
+			return ret;
+		}
+
+		Dispatch find = wordApp.call(selection, "Find").toDispatch();	// 从selection所在位置开始查询
+		Dispatch.put(find, "Text", text);			// 设置要查找的内容
+		Dispatch.put(find, "Forward", "True");		// 向前查找
+		Dispatch.put(find, "Format", "True");		// 设置格式
+		Dispatch.put(find, "MatchCase", "True");	// 大小写匹配
+		Dispatch.put(find, "MatchWholeWord", "True");	// 全字匹配
+		ret = Dispatch.call(find, "Execute").getBoolean();	// 查找并选中
+		return ret;
+	}
 }
