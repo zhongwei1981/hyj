798bdbd1eb4ea7f0fd29d0d278b576819b9af6e1
zhongwei <wei.zhong@etix.com>
2019-08-02 18:10:44 +0800

    op word init

---
+++ poi/README.txt
@@ -0,0 +1,50 @@
+##https://poi.apache.org/apidocs/index.html?org/apache/poi/openxml4j/opc/internal/package-summary.html
+##https://poi.apache.org/apidocs/3.17/
+
+##https://www.cnblogs.com/gdwkong/p/8669220.html
+一、POI概述
+POI结构:
+	POIFS - 此组件是所有其他POI元素的基本因素。 它用于显式读取不同的文件。
+	HPSF - 用于提取MS-Office文件的属性集。
+	HSLF － 提供读写Microsoft PowerPoint格式档案的功能。
+	HDGF － 提供读写Microsoft Visio格式档案的功能, 包含MS-Visio二进制文件的类和方法。 HDGF=Horrible DiaGram格式
+	HPBF - 用于读取和写入MS-Publisher文件。 HPBF=Horrible Publisher格式
+-- 2007, Microsoft OLE 2复合文档格式, Microsoft Word 97 - 2007
+	HSSF － 提供读写Microsoft Excel格式档案(*.xls)的功能。
+	HWPF － 提供读写Microsoft Word格式档案(*.doc)的功能。
+-- 2007+, OOXML (Office Open XML)
+	XSSF － 提供读写Microsoft Excel OOXML格式档案(*.xlsx)的功能。
+	XWPF － 提供读写Microsoft Word OOXML格式档案(*.docx)的功能。 XWPF=XML字处理器格式.
+
+POI 3.17版本是支持jdk6的最后版本
+
+二、HSSF概况
+	HSSF 是Horrible SpreadSheet Format的缩写，通过HSSF，你可以用纯Java代码来读取、写入、修改Excel文件。
+	HSSF 为读取操作提供了两类API：
+		user model: 用户模型
+		event user model: 事件-用户模型
+
+三、 POI EXCEL文档结构类
+	HSSFWorkbook		excel文档对象
+	HSSFSheet			excel的sheet
+	HSSFRow				excel的行
+	HSSFCell			excel的单元格
+	HSSFFont			excel字体
+	HSSFName			名称
+	HSSFDataFormat		日期格式
+	HSSFHeader			sheet头
+	HSSFFooter			sheet尾
+	HSSFCellStyle		cell样式
+	HSSFDateUtil		日期
+	HSSFPrintSetup		打印
+	HSSFErrorConstants	错误信息表
+
+##https://blog.csdn.net/u010770896/article/details/79863477		- OOXML Documents
+Range：	它表示一个范围，这个范围可以是整个文档，也可以是里面的某一小节（Section），也可以是某一个段落（Paragraph），还可以是拥有共同属性的一段文本（CharacterRun）。
+	Section：		word文档的一个小节，一个word文档可以由多个小节构成。
+	Paragraph：		word文档的一个段落，一个小节可以由多个段落构成。
+	CharacterRun：	具有相同属性的一段文本，一个段落可以由多个CharacterRun组成。
+	Section、Paragraph、CharacterRun和Table都继承自Range
+Table：	一个表格。
+	TableRow：	表格对应的行。
+	TableCell：	表格对应的单元格。
--- poi/src/main/java/com/hyj/poi/App.java
+++ poi/src/main/java/com/hyj/poi/App.java
@@ -1,11 +1,118 @@
 package com.hyj.poi;
 
-import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.hwpf.usermodel.Bookmark;
+import org.apache.poi.hwpf.usermodel.Bookmarks;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import com.hyj.poi.word.OOXML.MSWordTool;
+
+/**
+ * HSSFWorkbook			excel文档对象
+ * HSSFSheet			excel的sheet
+ * HSSFRow				excel的行
+ * HSSFCell				excel的单元格
+ * HSSFFont				excel字体
+ * HSSFName				名称
+ * HSSFDataFormat		日期格式
+ * HSSFHeader			sheet头
+ * HSSFFooter			sheet尾
+ * HSSFCellStyle		cell样式
+ * HSSFDateUtil			日期
+ * HSSFPrintSetup		打印
+ * HSSFErrorConstants	错误信息表
+ *
+ * @author ETIX-2018-2
+ */
 public class App {
 	private static Logger log = Logger.getLogger(App.class.getName());
 
-	public static void main(String[] args) {
+	private static String TEST_EXCEL = "D:\\zhongwei\\hyj\\test.xls";
+	private static String sheetName = "Sheet1";
+
+	private static String TEST_WORD = "D:\\zhongwei\\hyj\\jacobTest.doc";
+	private static String TEST_WORD_1 = "D:\\zhongwei\\hyj\\jacobTest_1.doc";
+	private static String key = "##主题##";
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		log.info("## Hello World!");
+
+		// Excel
+		HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(TEST_EXCEL));
+		HSSFSheet sheetAt = workbook.getSheet(sheetName);
+		HSSFCell cell = sheetAt.getRow(1).getCell(1);
+		CellType type = cell.getCellTypeEnum();
+		switch (type) {
+		case NUMERIC:
+			log.info("#### (1, 1) NUMERIC = " + cell.getNumericCellValue());
+			break;
+		case STRING:
+			log.info("#### (1, 1) STRING = " + cell.getStringCellValue());
+			break;
+		default:
+			log.error("#### not supported type=" + type);
+		}
+		workbook.close();
+
+		// Word -- OLE2
+		readWordFile(TEST_WORD);
+
+		// Word -- OOXML
+		if (false) {
+			MSWordTool changer = new MSWordTool();
+			changer.setTemplate(TEST_WORD);
+			Map<String,String> content = new HashMap<>();
+			content.put(key, "3.14");
+			changer.replaceBookMark(content);
+			changer.saveAs(TEST_WORD_1);
+		}
+	}
+
+	public static <T> List<String> readWordFile(String path) throws FileNotFoundException, IOException {
+		List<String> contextList = new ArrayList<>();
+		InputStream stream = null;
+		stream = new FileInputStream(new File(path));
+		if (path.endsWith(".doc")) {
+			HWPFDocument document = new HWPFDocument(stream);
+			log.info("#### 0. " + document.getDocumentText());
+			Bookmarks bms = document.getBookmarks();
+			log.info("#### 1. " + bms.getBookmarksCount());
+			for(int i = 0; i < bms.getBookmarksCount(); i++) {
+				Bookmark bm = bms.getBookmark(i);
+				log.info(String.format("#### 2. %s, %d, %d", bm.getName(), bm.getStart(), bm.getEnd()));
+			}
+			WordExtractor extractor = new WordExtractor(document);
+			String[] contextArray = extractor.getParagraphText();
+//			Arrays.asList(contextArray).forEach(context -> contextList.add(CharMatcher.whitespace().removeFrom(context)));
+			extractor.close();
+			document.close();
+		} else if (path.endsWith(".docx")) {
+			XWPFDocument document = new XWPFDocument(stream).getXWPFDocument();
+			List<XWPFParagraph> paragraphList = document.getParagraphs();
+//			paragraphList.forEach(paragraph -> contextList.add(CharMatcher.whitespace().removeFrom(paragraph.getParagraphText())));
+			document.close();
+		} else {
+			log.info("not word doc:" + path);
+		}
+
+		stream.close();
+		return contextList;
 	}
 }
